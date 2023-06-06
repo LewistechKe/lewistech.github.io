5090ea083ec23fdb38b7a199259b309112b29187
Lewis Muthaura <lewislemmu10@gmail.com>
2023-06-06 22:32:46 +0300

    first commit

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Lewis Portfolio - Mathematics and Computer Science Student</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Lewis Portfolio - Mathematics and Computer Science Student" />
+            <meta name="description"
+                content="Lewis Muthaura's (lewistech) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Lewis Muthaura (lewistech)" />
+            <meta name="keywords"
+                content="LewistechKe, lewistechKe's portfolio, lewistech linux, ubuntu portfolio, lewis muthaura protfolio,lewistech computer, Lewis Muthaura, lewis ubuntu, lewis muthaura ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.ico" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Lewis Muthaura Portfolio - Mathematics and Computer Science Student" />
+            <meta itemProp="description"
+                content="Lewis Muthaura(lewistech) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Lewis Muthaura Portfolio - Mathematics and Computer Science Student" />
+            <meta name="twitter:description"
+                content="Lewis Muthaura (lewistech) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="Lewistech Ke" />
+            <meta name="twitter:creator" content="Lewistech Ke" />
+            <meta name="twitter:image:src" content="images/logos/fevicon.ico" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Lewis Muthaura Portfolio - Mathematics and Computer Science Student" />
+            <meta name="og:description"
+                content="Lewis Muthaura (lewistech) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/favicon.ico" />
+            <meta name="og:url" content="http://lewistech.github.io/" />
+            <meta name="og:site_name" content="Lewis Muthaura Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.ico" />
+            <link rel="apple-touch-icon" href="images/logos/fevicon.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
